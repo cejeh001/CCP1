1b062b49745d0841a903c524f5fab091907a7b48
Chibueze <149211316+Mel0nWater@users.noreply.github.com>
2025-03-02 00:03:06 +0000

    Revert "attempt 1"
    This reverts commit 34b8fec5bfbc0a6036fb0d3d3d6b270dd61ea7f5.

--- script.js
+++ script.js
@@ -1,16 +1,6 @@
 let penColor = 'lightblue';
 let penSize = 10;
 
-// variables for dragging canvas
-let drag = false;
-let offsetX = 0;
-let offsetY = 0;
-let canvasX = 0;
-let canvasY = 0;
-
-// variables for drawing mode
-let drawingMode = true;
-
 // snowman images
 let snowman, snowmanMouthOpen, snowmanChewing, snowmanSpitOut, snowmanHoldingBelly;
 
@@ -18,53 +8,18 @@ function setup() {
   createCanvas(500, 500);
   background(255);
   angleMode(DEGREES);
-
-  // Create a button to toggle drawing and dragging
-  let button = createButton('Drawing/Dragging');
-  button.position(10, 10);
-  button.mousePressed(drawingMode);
 }
 
 function draw() {
-  if (drawingMode) {
-    if (mouseIsPressed) {
-      stroke(penColor);
-      strokeWeight(penSize);
-      line(mouseX - canvasX, mouseY - canvasY, pmouseX - canvasX, pmouseY - canvasY);
+  if (mouseIsPressed) {
+    stroke(penColor);
+    strokeWeight(penSize);
+    line(mouseX, mouseY, pmouseX, pmouseY);
 
-      translate(width / 2, height / 2);
-      for (let i = 0; i < 360; i += 30) {
-        rotate(i);
-        line(mouseX - canvasX - width / 2, mouseY - canvasY - height / 2, pmouseX - canvasX - width / 2, pmouseY - canvasY - height / 2);
-      }
-    }
-  } else {
-    if (mouseIsPressed) {
-      drag = true;
-      offsetX = mouseX - canvasX;
-      offsetY = mouseY - canvasY;
+    translate(width / 2, height / 2);
+    for (let i = 0; i < 360; i += 30) {
+      rotate(i);
+      line(mouseX - width / 2, mouseY - height / 2, pmouseX - width / 2, pmouseY - height / 2);
     }
   }
-
-  background(255); // Clear the canvas with a white background
-  translate(canvasX, canvasY);
-  fill(255);
-  rect(0, 0, 500, 500);
-}
-
-function mouseDragged() {
-  if (!drawingMode && drag) {
-    canvasX = mouseX - offsetX;
-    canvasY = mouseY - offsetY;
-  }
-}
-
-function mouseReleased() {
-  if (!drawingMode) {
-    drag = false;
-  }
-}
-
-function toggleMode() {
-  drawingMode = !drawingMode;
 }
